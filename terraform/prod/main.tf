7fd64184b63a0a60e15c983913656f2c189aeb77
Terentev Kirill <terentevkv@ekassir.com>
2021-10-10 18:12:42 +0300

    add some var to terraform

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -11,6 +11,8 @@ module "app" {
   app_disk_image  = var.app_disk_image
   subnet_id       = var.subnet_id
   app_instances_count = var.app_instances_count
+  database_url     = module.db.local_ip_address_db
+  private_key_for_conn_provisioner = var.private_key_for_conn_provisioner
 }
 
 module "db" {
